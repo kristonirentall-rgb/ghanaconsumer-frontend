49316bc1343147447bd4e6048175708c82b75271
Philip Arthur <philiparthur@Philips-MacBook-Air.local>
2025-09-01 22:01:27 +0000

    fix: remove mongoose import from frontend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 ensures correct absolute paths
   server: {
     proxy: {
       '/api': {
